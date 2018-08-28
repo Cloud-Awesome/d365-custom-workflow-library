4600d63d4151559690ab89b6dd1e5a471dc98817
Arthur Nicholson <arthur@thebi.ninja>
2018-08-28 13:55:54 +0100

    Initial commit

---
+++ CustomWorkflowLibrary.DataConversion.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomWorkflowLibrary.DataConversion.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomWorkflowLibrary.DataConversion.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("faa6a93f-2fc9-48c9-ac8f-15c501d999ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,18 @@
+# Custom Workflow Library
+
+A set of custom workflow steps for Dynamics 365. 
+
+To save having to import all steps, the library is broken into multiple managed solutions grouped by function:
+
+- Data type conversions
+- Date and time functions
+- CRM platform functions
+- String manipulation
+- RegEx functions
+- Mathematical operations
+- Data quality and validation
+- Web services
+- Sharepoint integration
+- Azure integration
+
+Built and tested for v9.0 online only.
