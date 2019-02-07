ac567429f7d7645ed9778e7e53c01f726f791be0
Arthur Nicholson <arthur@thebi.ninja>
2019-02-07 10:05:53 +0000

    Added Trim (String Manipulation) CWA

--- StringManipulation/Trim.cs
+++ StringManipulation/Trim.cs
@@ -1,9 +1,10 @@
-﻿namespace StringManipulation
-{
-    public class Trim
-    {
-        //TODO - WorkFlow Activity
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
 
+namespace StringManipulation
+{
+    public class Trim: CodeActivity
+    {
         public string GetTrimmedString(string inputString, string stringToTrim)
         {
             var returnValue = !string.IsNullOrEmpty(stringToTrim) ? 
@@ -11,6 +12,26 @@
                 inputString.Trim();
 
             return returnValue;
+        }
+
+        [Input("Input String")]
+        [RequiredArgument]
+        public InArgument<string> StringInArgument { get; set; }
+
+        [Input("Char to Trim (leave blank to trim space)")]
+        public InArgument<string> StringToTrimInArgument { get; set; }
+
+        [Output("Trimmed String")]
+        public OutArgument<string> TrimmedStringOutArgument { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var inputString = StringInArgument.Get(context);
+            var stringToTrim = StringToTrimInArgument.Get(context);
+
+            var returnValue = GetTrimmedString(inputString, stringToTrim);
+
+            TrimmedStringOutArgument.Set(context, returnValue);
         }
     }
 }
