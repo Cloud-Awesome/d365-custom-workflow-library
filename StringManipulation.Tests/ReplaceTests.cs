0098000ef593fd0cc5aede2d00f93dabcf3df1f7
Arthur Nicholson <arthur@thebi.ninja>
2019-02-07 11:06:10 +0000

    Added Replace and SetCase activities

---
+++ StringManipulation.Tests/ReplaceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace StringManipulation.Tests
+{
+    [TestFixture]
+    public class ReplaceTests
+    {
+        [Test]
+        [TestCase("Replace String", "Replace", "Replaced", "Replaced String")]
+        [TestCase("Replace String", "...", "Nothing", "Replace String")]
+        public void HappyPath(string inputString, string oldString, string newString, string expectedResult)
+        {
+            var test = new Replace();
+            var result = test.GetReplacedString(inputString, oldString, newString);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ StringManipulation/SetCase.cs
@@ -0,0 +1,43 @@
+﻿using System.Activities;
+using System.Globalization;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace StringManipulation
+{
+    public class SetCase: CodeActivity
+    {
+        public string GetSetCase(string inputString, string caseOption)
+        {
+            switch (caseOption.ToLower())
+            {
+                case "lower":
+                    return inputString.ToLower();
+                case "upper":
+                    return inputString.ToUpper();
+                default: //Proper
+                    var cultureInfo = CultureInfo.CurrentCulture;
+                    var textInfo = cultureInfo.TextInfo;
+                    return textInfo.ToTitleCase(inputString);
+            }
+        }
+
+        [Input("Input String")]
+        public InArgument<string> StringInArgument { get; set; }
+
+        [Input("Case: 'UPPER', 'lower' or 'Proper'")]
+        public InArgument<string> CaseOptionInArgument { get; set; }
+
+        [Output("Return String")]
+        public OutArgument<string> StringOutArgument { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var inputString = StringInArgument.Get(context);
+            var caseOption = CaseOptionInArgument.Get(context);
+
+            var returnString = GetSetCase(inputString, caseOption);
+
+            StringOutArgument.Set(context, returnString);
+        }
+    }
+}
