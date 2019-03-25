79c3f19cd715df404b1af20b4fec1ba0ae044200
Arthur Nicholson <arthur@iscloudnative.com>
2019-03-25 16:24:08 +0000

    Adds two workflow steps to add and remove users to/from teams. Manually tested (still requires automated tests). Unit and Integration Test projects added.

---
+++ CrmPlatform/AddUserToTeam.cs
@@ -0,0 +1,45 @@
+﻿using System.Activities;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace CrmPlatform
+{
+    public class AddUserToTeam: CodeActivity
+    {
+        [Input("User to add")]
+        [ReferenceTarget("systemuser")]
+        [RequiredArgument]
+        public InArgument<EntityReference> UserToAddInArgument { get; set; }
+
+        [Input("Team to join")]
+        [ReferenceTarget("team")]
+        [RequiredArgument]
+        public InArgument<EntityReference> TeamToJoinInArgument { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext executionContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(executionContext.UserId);
+
+            var userToAdd = UserToAddInArgument.Get(context);
+            var teamToJoin = TeamToJoinInArgument.Get(context);
+
+            var result = AddUser(userToAdd, teamToJoin, service);
+        }
+
+        private ParameterCollection AddUser(EntityReference user, EntityReference team, IOrganizationService service)
+        {
+            var request = new AddMembersTeamRequest
+            {
+                MemberIds = new[] { user.Id },
+                TeamId = team.Id
+            };
+            
+            var response = (AddMembersTeamResponse)service.Execute(request);
+            return response.Results;
+        }
+
+    }
+}
---
+++ CrmPlatform/RemoveUserFromTeam.cs
@@ -0,0 +1,44 @@
+﻿using System.Activities;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace CrmPlatform
+{
+    public class RemoveUserFromTeam: CodeActivity
+    {
+        [Input("User to remove")]
+        [ReferenceTarget("systemuser")]
+        [RequiredArgument]
+        public InArgument<EntityReference> UserToRemoveInArgument { get; set; }
+
+        [Input("Team to leave")]
+        [ReferenceTarget("team")]
+        [RequiredArgument]
+        public InArgument<EntityReference> TeamToLeaveInArgument { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext executionContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(executionContext.UserId);
+
+            var userToAdd = UserToRemoveInArgument.Get(context);
+            var teamToJoin = TeamToLeaveInArgument.Get(context);
+
+            var result = RemoveUser(userToAdd, teamToJoin, service);
+        }
+
+        private ParameterCollection RemoveUser(EntityReference user, EntityReference team, IOrganizationService service)
+        {
+            var request = new RemoveMembersTeamRequest
+            {
+                MemberIds = new[] { user.Id },
+                TeamId = team.Id
+            };
+
+            var response = (RemoveMembersTeamResponse)service.Execute(request);
+            return response.Results;
+        }
+    }
+}
