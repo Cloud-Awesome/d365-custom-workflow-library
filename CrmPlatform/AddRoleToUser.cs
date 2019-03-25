0075fad3c94fc215f8f3793a53de1cd2e64e404c
Arthur Nicholson <arthur@iscloudnative.com>
2019-03-25 17:53:29 +0000

    Added two steps to add/remove a security role to/from systemuser. Manully tested. Requires automated testing.

---
+++ CrmPlatform/AddRoleToUser.cs
@@ -0,0 +1,41 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace CrmPlatform
+{
+    public class AddRoleToUser: CodeActivity
+    {
+
+        [Input("User")]
+        [ReferenceTarget("systemuser")]
+        [RequiredArgument]
+        public InArgument<EntityReference> UserInArgument { get; set; }
+
+        [Input("Security role to add")]
+        [ReferenceTarget("role")]
+        [RequiredArgument]
+        public InArgument<EntityReference> RoleToAddInArgument { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext executionContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(executionContext.UserId);
+
+            var user = UserInArgument.Get(context);
+            var role = RoleToAddInArgument.Get(context);
+
+            AddRole(user, role, service);
+        }
+
+        private void AddRole(EntityReference user, EntityReference role, IOrganizationService service)
+        {
+            service.Associate("systemuser", user.Id,
+                new Relationship("systemuserroles_association"), new EntityReferenceCollection()
+                {
+                    new EntityReference(role.LogicalName, role.Id)
+                });
+        }
+    }
+}
---
+++ CrmPlatform/RemoveRoleFromUser.cs
@@ -0,0 +1,41 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace CrmPlatform
+{
+    public class RemoveRoleFromUser: CodeActivity
+    {
+        [Input("User")]
+        [ReferenceTarget("systemuser")]
+        [RequiredArgument]
+        public InArgument<EntityReference> UserInArgument { get; set; }
+
+        [Input("Security role to remove")]
+        [ReferenceTarget("role")]
+        [RequiredArgument]
+        public InArgument<EntityReference> RoleToRemoveInArgument { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext executionContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(executionContext.UserId);
+
+            var user = UserInArgument.Get(context);
+            var role = RoleToRemoveInArgument.Get(context);
+
+            RemoveRole(user, role, service);
+        }
+
+        private void RemoveRole(EntityReference user, EntityReference role, IOrganizationService service)
+        {
+            service.Disassociate("systemuser", user.Id, 
+                new Relationship("systemuserroles_association"),
+                new EntityReferenceCollection()
+                {
+                    new EntityReference(role.LogicalName, role.Id)
+                });
+        }
+    }
+}
